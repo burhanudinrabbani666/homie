e452611ec891c753c67ec14c4d4034da6d06657f
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-01-15 14:56:21 +0700

    feat: get data from form

--- new/src/js/script.js
+++ new/src/js/script.js
@@ -1,16 +1,54 @@
+import { initialData } from "../../../src/js/data/data.js";
+
 const favoritesBtnElement = document.querySelector(".form__btn--favorites");
-const  = document.querySelector(".form__btn--favorites");
+const formElement = document.querySelector(".form");
 
-
-let favorites = false;
+let favoritesValue = false;
 function newContactForm() {
   favoritesBtnElement.addEventListener("click", () => {
-    favorites = !favorites;
+    favoritesValue = !favoritesValue;
 
-    renderFavorites(favorites);
+    renderFavorites(favoritesValue);
   });
 
+  console.log(initialData.length + 1);
+  formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log("hello");
 
+    const data = new FormData(formElement);
+
+    const newContact = {
+      id: initialData.length + 1,
+      name: data.get("name").toString().trim(),
+      phone: data.get("phone").toString().trim(),
+      email: data.get("email").toString().trim(),
+      labels: [
+        {
+          labelName: "family",
+          color: "green",
+        },
+      ],
+      birthDate: data.get("birthdate") ? new Date(data.get("birthdate")) : null,
+      company: data.get("company").toString().trim() || null,
+      address: {
+        street: data.get("street").toString().trim() || null,
+        subdistrict: data.get("subdistrict").toString().trim() || null,
+        city: data.get("city").toString().trim() || null,
+        province: data.get("province").toString().trim() || null,
+        country: data.get("country").toString().trim() || null,
+        zipCode: data.get("zipCode").toString().trim() || null,
+      },
+      createdAt: new Date(),
+      updatedAt: [{ id: 1, date: new Date() }],
+      deletedAt: null,
+      favorites: favoritesValue,
+      photoProfileLink: data.get("photoProfile").toString().trim() || null,
+      backgroundLink: data.get("bgImageLink").toString().trim() || null,
+    };
+
+    console.log(newContact);
+  });
 }
 
 newContactForm();
